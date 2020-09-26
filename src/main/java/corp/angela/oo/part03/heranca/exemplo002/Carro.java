67191ce280870d7433f6f4cb316468d753ec884f
Angela Crepaldi <angelacrepaldi@gmail.com>
2020-09-26 18:37:24 -0300

    Cloned examples of Object Oriented and handling Dates.

---
+++ src/main/java/corp/angela/oo/part03/heranca/exemplo002/Carro.java
@@ -0,0 +1,14 @@
+package corp.angela.oo.part03.heranca.exemplo002;
+
+public class Carro extends Veiculo {
+
+    private int quantidadeDePortas;
+
+    public int getQuantidadeDePortas() {
+        return quantidadeDePortas;
+    }
+
+    public void setQuantidadeDePortas(int quantidadeDePortas) {
+        this.quantidadeDePortas = quantidadeDePortas;
+    }
+}
